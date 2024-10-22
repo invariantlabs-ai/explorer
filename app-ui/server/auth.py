33aaefe5670de5a7533bf1202ff81c385a534055
Luca Beurer-Kellner <lucabeurerkellner@gmail.com>
2024-10-22 14:08:31 +0200

    configure auth in config files

--- app-ui/server/auth.py
+++ app-ui/server/auth.py
@@ -7,10 +7,11 @@ from fastapi import HTTPException
 from keycloak import KeycloakOpenID # pip require python-keycloak
 import os
 from typing import Annotated
+from util.config import config
 
 is_preview_deployment = os.getenv("PREVIEW") == "1"
 base_url = "https://" + os.getenv("APP_NAME") + ".invariantlabs.ai"
-client_id = "invariant-" + os.getenv("APP_NAME")
+client_id = config("authentication_client_id_prefix") + "-" + os.getenv("APP_NAME")
 
 if is_preview_deployment:
     client_id = client_id.replace("preview-", "")
@@ -18,13 +19,9 @@ if is_preview_deployment:
 keycloak_openid = KeycloakOpenID(
     server_url="https://auth.invariantlabs.ai/",
     client_id=client_id,
-    realm_name="invariant-public",
-    client_secret_key="GDq4AqlO9jWiZJ5HgIIdfhgxHyhVZnTG"
+    realm_name=config("authentication_realm"),
+    client_secret_key=os.getenv("KEYCLOAK_CLIENT_ID_SECRET")
 )
-
-
-AUTHORIZATION_URL = "https://auth.invariantlabs.ai/realms/invariant/protocol/openid-connect/auth",
-TOKEN_URL = "https://auth.invariantlabs.ai/realms/invariant/protocol/openid-connect/token",
 
 def install_authorization_endpoints(app):
     """
