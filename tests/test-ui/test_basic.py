3f4f72c5f89a5ada61b796efc29723977950da10
Zishan <zishan@Zishans-MacBook-Pro.local>
2024-12-19 16:54:37 +0100

    skip the test in ci

--- tests/test-ui/test_basic.py
+++ tests/test-ui/test_basic.py
@@ -8,7 +8,6 @@ from playwright.async_api import expect
 from typing import Literal
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-# os.chdir(os.path.dirname(os.getcwd()))
 
 import tempfile
 
