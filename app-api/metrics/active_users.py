a3370c653460a2eda4a7d544b52bff358e8f76dd
Hemang <hemang@invariantlabs.ai>
2025-01-10 09:32:21 +0100

    Format app-api code.

--- app-api/metrics/active_users.py
+++ app-api/metrics/active_users.py
@@ -1,14 +1,11 @@
 """
 Prometheus count and middleware to track active users (anonymous or authenticated) in the last 5 minutes.
 """
-import datetime
-from typing import Annotated
 
-from prometheus_fastapi_instrumentator import metrics
-from prometheus_client import Gauge
+import datetime
 
 from fastapi import Request
-
+from prometheus_client import Gauge
 from routes.auth import UserIdentity
 
 # the gauge tracks active users (active within the last 5 minutes)
@@ -26,9 +23,10 @@ active_anonymous_users = Gauge(
     subsystem="explorer",
 )
 
+
 # set that automatically removes elements after a certain time
 class max_live_set:
-    def __init__(self, max_live=5*60):
+    def __init__(self, max_live=5 * 60):
         self.elements = {}
         self.max_live = max_live
 
@@ -39,11 +37,11 @@ class max_live_set:
     def __contains__(self, element):
         self.cleanup(self.max_live)
         return element in self.elements
-    
+
     def __len__(self):
         self.cleanup(self.max_live)
         return len(self.elements)
-    
+
     def __iter__(self):
         self.cleanup(self.max_live)
         return iter(self.elements)
@@ -51,33 +49,39 @@ class max_live_set:
     def cleanup(self, timeout):
         sorted_elements = sorted(self.elements.items(), key=lambda x: x[1])
         for element, timestamp in sorted_elements:
-            if datetime.datetime.now() - timestamp > datetime.timedelta(seconds=timeout):
+            if datetime.datetime.now() - timestamp > datetime.timedelta(
+                seconds=timeout
+            ):
                 del self.elements[element]
             else:
                 break
 
+
 # set of active users (anonymous or authenticated)
 ACTIVE_USERS = max_live_set()
 ACTIVE_ANONYMOUS_USERS = max_live_set()
+
 
 def install_metrics_middleware(app):
     @app.middleware("http")
     async def count_active_users(request: Request, call_next):
         try:
             userinfo = await UserIdentity(request)
-        except Exception as e:
+        except Exception:
             userinfo = {}
-        
-        userid = userinfo.get("sub") or request.headers.get("x-forwarded-for", "anonymous")
-        
+
+        userid = userinfo.get("sub") or request.headers.get(
+            "x-forwarded-for", "anonymous"
+        )
+
         # ignore the /metrics endpoint
         if not request.url.path.endswith("/metrics"):
             ACTIVE_USERS.add(userid)
             # track anonymous users in a separate set
             if userinfo.get("sub") is None:
                 ACTIVE_ANONYMOUS_USERS.add(userid)
-        
+
         active_users.set(len(ACTIVE_USERS))
         active_anonymous_users.set(len(ACTIVE_ANONYMOUS_USERS))
-        
+
         return await call_next(request)
--- app-api/util/validation.py
+++ app-api/util/validation.py
@@ -1,40 +1,49 @@
 import json
-from models.datasets_and_traces import Trace, Annotation
 from typing import Any
+
+from models.datasets_and_traces import Annotation, Trace
+
 
 class ValidationError(Exception):
     pass
 
+
 class AnnotationValidationError(ValidationError):
     pass
+
 
 class TraceValidationError(ValidationError):
     pass
 
+
 def validate_trace(trace: Trace) -> bool:
     """Validates format of the trace."""
     return True
+
 
 def validate_annotation(annotation: Annotation, trace: Trace) -> bool:
     """Validates format of the annotation based on the corresponding trace.
     We parse the address of the annotation and always ensure we can find the corresponding element in the trace,
     throwing an exception otherwise. Content of the annotation can be arbitrary string.
     """
+
     def move_index(curr_el: Any, index: str) -> Any:
         if index.isdigit():
             index = int(index)
         else:
             raise AnnotationValidationError(f"Index {index} is not an integer")
         if not isinstance(curr_el, list):
-            raise AnnotationValidationError(f"Trying to access index of a non-list")
+            raise AnnotationValidationError("Trying to access index of a non-list")
         if index >= len(curr_el):
-            raise AnnotationValidationError(f"Index {index} out of bounds for key {key}")
+            raise AnnotationValidationError(
+                f"Index {index} out of bounds for key {key}"
+            )
         curr_el = curr_el[index]
         return curr_el
-    
+
     def move_key(curr_el: Any, key: str) -> Any:
         if not isinstance(curr_el, dict):
-            raise AnnotationValidationError(f"Trying to access key of a non-dict")
+            raise AnnotationValidationError("Trying to access key of a non-dict")
         if key not in curr_el:
             raise AnnotationValidationError(f"Key {key} not found in {curr_el}")
         curr_el = curr_el[key]
@@ -44,16 +53,16 @@ def validate_annotation(annotation: Annotation, trace: Trace) -> bool:
     curr_el = {"messages": json.loads(trace.content)}
     for chunk in address_chunks:
         if ":" in chunk:
-            key = chunk[:chunk.index(":")]
+            key = chunk[: chunk.index(":")]
             curr_el = move_key(curr_el, key).split("\n")
-            index = chunk[chunk.index(":")+2:]
+            index = chunk[chunk.index(":") + 2 :]
             index = move_index(curr_el, index)
         elif "[" in chunk:
             if chunk[-1] != "]":
                 raise AnnotationValidationError(f"Index in {chunk} not closed")
-            key = chunk[:chunk.index("[")]
+            key = chunk[: chunk.index("[")]
             curr_el = move_key(curr_el, key)
-            index = chunk[chunk.index("[")+1:-1]
+            index = chunk[chunk.index("[") + 1 : -1]
             curr_el = move_index(curr_el, index)
         else:
             curr_el = move_key(curr_el, chunk)
