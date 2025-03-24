58e2300f79807013b3baba185b2c42bab77938ca
Luca Beurer-Kellner <lucabeurerkellner@gmail.com>
2025-03-24 21:08:36 +0100

    ignore user identity requests

--- app-api/metrics/active_users.py
+++ app-api/metrics/active_users.py
@@ -72,8 +72,10 @@ def install_metrics_middleware(app):
 
         userid = user_id or request.headers.get("x-forwarded-for", "anonymous")
 
-        # ignore the /metrics endpoint
-        if not request.url.path.endswith("/metrics"):
+        # ignore the /metrics endpoint and user identity checks by other services
+        if not request.url.path.endswith("/metrics") and not request.url.path.endswith(
+            "/user/identity"
+        ):
             ACTIVE_USERS.add(userid)
             # track anonymous users in a separate set
             if user_id is None:
