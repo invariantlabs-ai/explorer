efbcb31967c1f2db051e3427d8570660997b4a37
Marc Fischer <mail@marcfischer.at>
2025-04-07 14:20:12 +0200

    css alignment fix

--- app-ui/src/pages/playground/spinning.tsx
+++ app-ui/src/pages/playground/spinning.tsx
@@ -7,7 +7,6 @@ function Spinning({ className }: { className?: string }) {
       viewBox="0 0 24 24" 
       width="1em" 
       height="1em"
-      style={{ verticalAlign: 'middle' }}
     >
       <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
       <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"></path>
