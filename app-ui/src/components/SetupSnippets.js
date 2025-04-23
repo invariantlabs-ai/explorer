41caccb54a16c8a8588047b85061108c193e4f91
Marco Milanta <marco.milanta@gmail.com>
2025-04-23 14:26:05 +0200

    feat: add examples and fix css

--- app-ui/src/components/SetupSnippets.js
+++ app-ui/src/components/SetupSnippets.js
@@ -10,26 +10,30 @@ export const SETUP_SNIPPETS = [
     name: "OpenAI",
     description: "Upload via Gateway and the OpenAI Python SDK.",
     link: "https://explorer.invariantlabs.ai/docs/gateway/llm-provider-integrations/openai/",
-    snippet: (dataset, instance) => `from openai import OpenAI
-from httpx import Client
+    snippetPerLanguage: {
+      python: (dataset, instance) => `from openai import OpenAI
 import os
 
-http_client = Client(
-  headers={
-      "Invariant-Authorization": "Bearer " + os.getenv("INVARIANT_API_KEY"),
-  },
-)
 openai_client = OpenAI(
-  http_client=http_client,
-  base_url="${instance}/api/v1/gateway/${dataset}/openai",
+    api_key=os.getenv("OPENAI_API_KEY"),
+    base_url="${instance}/api/v1/gateway/${dataset}/openai",
+    default_headers={"Invariant-Authorization": "Bearer " + os.getenv("INVARIANT_API_KEY")},
 )`,
-  },
+      typescript: (dataset, instance) => `import OpenAI from 'openai';
+
+const client = new OpenAI({
+  apiKey: process.env['OPENAI_API_KEY'],
+  baseURL: "${instance}/api/v1/gateway/${dataset}/openai",
+  defaultHeaders: {"Invariant-Authorization": "Bearer " + process.env['INVARIANT_API_KEY'],},
+});`
+},
+},
   {
     name: "MCP",
     description:
       "Use Invariant via its MCP Gateway, to guardrail and log your MCP server interactions.",
     link: "https://explorer.invariantlabs.ai/docs/",
-    snippet: (dataset, instance) => `{
+    snippetPerLanguage: {json: (dataset, instance) => `{
  "you-mcp-server": {
   "command": "uvx",
   "args": [
@@ -45,31 +49,33 @@ openai_client = OpenAI(
     "INVARIANT_API_KEY": "<INVARIANT_API_KEY>",${!instance.includes("https://explorer.invariantlabs.ai") ? '\n    "INVARIANT_API_ENDPOINT": "' + instance + '",' : ""}
   }
 }
-`,
+`},
   },
   {
     name: "Anthropic",
     description: "Upload via Gateway and the Anthropic Python SDK.",
     link: "https://explorer.invariantlabs.ai/docs/gateway/llm-provider-integrations/anthropic/",
-    snippet: (dataset, instance) => `from anthropic import Anthropic
-from httpx import Client
+    snippetPerLanguage: {python: (dataset, instance) => `from anthropic import Anthropic
 import os
 
-http_client = Client(
-  headers={
-    "Invariant-Authorization": "Bearer "  + os.getenv("INVARIANT_API_KEY"),
-  },
-)
 anthropic_client = Anthropic(
-  http_client=http_client,
-  base_url="${instance}/api/v1/gateway/${dataset}/anthropic",
+    api_key=os.getenv("ANTHROPIC_API_KEY"),
+    default_headers={"Invariant-Authorization": "Bearer " + os.getenv("INVARIANT_API_KEY")},
+    base_url="${instance}/api/v1/gateway/${dataset}/anthropic",
 )`,
-  },
+      typescript: (dataset, instance) => `import Anthropic from '@anthropic-ai/sdk';
+
+const anthropic = new Anthropic({
+  apiKey: process.env["ANTHROPIC_API_KEY"],
+  baseURL: "${instance}/api/v1/gateway/${dataset}/anthropic",
+  defaultHeaders: {"Invariant-Authorization": "Bearer " + process.env['INVARIANT_API_KEY'],},
+});`,
+}},
   {
     name: "Gemini",
     description: "Upload via Gateway and the Gemini Python SDK.",
     link: "https://explorer.invariantlabs.ai/docs/gateway/llm-provider-integrations/gemini/",
-    snippet: (dataset, instance) => `from google import genai
+    snippetPerLanguage: {python: (dataset, instance) => `from google import genai
 import os
 
 client = genai.Client(
@@ -81,15 +87,27 @@ client = genai.Client(
         },
     },
 )`,
-  },
+      typescript: (dataset, instance) => `import {GoogleGenAI} from '@google/genai';
+
+const gemini = new GoogleGenAI({
+    apiKey: process.env['GEMINI_API_KEY'],
+    httpOptions: {
+        baseUrl: '${instance}/api/v1/gateway/${dataset}/gemini',
+        headers: {
+            'Invariant-Authorization': "Bearer " + process.env['INVARIANT_API_KEY'],
+        },
+    }
+});`
+  }},
   {
     name: "Agents SDK",
     description: "Upload via Gateway and the OpenAI Agents SDK.",
     link: "https://explorer.invariantlabs.ai/docs/gateway/agent-integrations/openai-agents-sdk/",
-    snippet: (
-      dataset,
-      instance
-    ) => `from agents import Agent, OpenAIChatCompletionsModel
+    snippetPerLanguage: {
+      python: (
+        dataset,
+        instance
+      ) => `from agents import Agent, OpenAIChatCompletionsModel
 from openai import AsyncOpenAI
 import os
 
@@ -103,29 +121,29 @@ external_client = AsyncOpenAI(
 agent = Agent(
     name="Assistant", instructions="You are a helpful assistant",
     model=OpenAIChatCompletionsModel(model="gpt-4o", openai_client=external_client),
-)`,
+)`}
   },
   {
     name: "Swarm",
     description: "Upload via Gateway from within the Swarm framework.",
     link: "https://explorer.invariantlabs.ai/docs/gateway/agent-integrations/openai-swarm/",
-    snippet: (dataset, instance) => `from swarm import Swarm, Agent
+    snippetPerLanguage: {python: (dataset, instance) => `from swarm import Swarm, Agent
 from openai import OpenAI
 from httpx import Client
 import os
 
 client = Swarm(
   client=OpenAI(
-      http_client=Client(headers={"Invariant-Authorization": "Bearer " + os.getenv("INVARIANT_API_KEY", "")}),
-      base_url="${instance}/api/v1/gateway/${dataset}/openai",
+    base_url="${instance}/api/v1/gateway/${dataset}/openai",
+    default_headers={"Invariant-Authorization": "Bearer " + os.getenv("INVARIANT_API_KEY")},
   )
-)`,
+)`,}
   },
   {
     name: "Invariant SDK",
     description: "Use the Invariant SDK to manually upload traces.",
     link: "https://explorer.invariantlabs.ai/docs/explorer/api/uploading-traces/push-api/",
-    snippet: (dataset, instance) => `from invariant_sdk.client import Client
+    snippetPerLanguage: {python: (dataset, instance) => `from invariant_sdk.client import Client
 
 # requires the 'INVARIANT_API_KEY' environment variable to be set
 client = Client()
@@ -137,11 +155,11 @@ messages = [
     ]
 ]
 
-response = client.create_request_and_push_trace(messages=messages, dataset="${dataset}")`,
+response = client.create_request_and_push_trace(messages=messages, dataset="${dataset}")`,}
   },
   {
     name: "JSON Upload",
     description: "Upload a JSONL file with traces.",
-    snippet: "<jsonl-upload>",
+    snippetPerLanguage: {json: "<jsonl-upload>"},
   },
 ];
